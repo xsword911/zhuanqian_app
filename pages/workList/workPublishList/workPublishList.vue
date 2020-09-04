498af4093d25036759ad4ea6b9e8ff2630d4dfcb
beidaobadechuiyangliu <12121213344@qq.com>
2020-09-04 16:07:46 +0800

    1

--- pages/workList/workPublishList/workPublishList.vue
+++ pages/workList/workPublishList/workPublishList.vue
@@ -19,7 +19,7 @@
 				@tap="toUpdWork(item)">
 					<view class="info_left">
 						<view class="info_title">{{item.title}}</view>
-						<view class="info_time">
+<!-- 						<view class="info_time">
 							任务类型：
 							<text v-if="item.type == 0">邀请好友</text>
 							<text v-if="item.type == 1">分享朋友圈</text>
@@ -27,6 +27,11 @@
 							<text v-if="item.type == 3">下载app</text>
 							<text v-if="item.type == 4">签到任务</text>
 							<text v-if="item.type == 5">点赞任务</text>
+						</view> -->
+						<view class="info_time">
+							状态：
+							<text v-if="item.state == 0">关闭</text>
+							<text v-if="item.state == 1">开启</text>
 						</view>
 						<view class="info_time">{{item.begTime}}</view>
 					</view>
@@ -65,6 +70,7 @@
 						<input type="text" value="" @tap="show(1)" v-model="begTime" :disabled="true" />
 					</view>
 				</view>
+				
 				<view class="search_time">
 					<view class="search_test">
 						<text>结束时间</text>
@@ -73,6 +79,7 @@
 						<input type="text" value="" @tap="show(2)" v-model="endTime" :disabled="true" />
 					</view>
 				</view>
+				
 				<view class="search_time" v-if="arrayLevel.length > 0">
 					<view class="search_test">
 						<text>任务等级</text>
@@ -83,6 +90,18 @@
 						</picker>
 					</view>
 				</view>
+				
+				<view class="search_time" v-if="arrayLevel.length > 0">
+					<view class="search_test">
+						<text>状态</text>
+					</view>
+					<view class="uni-list-cell-db" style="border-bottom:1px solid #808080;">
+						<picker @change="statePickerChange" :value="arrayStateIndex" :range="arrayState" range-key="state" name="state">
+							<view class="uni-input">{{arrayState[arrayStateIndex].state}}</view>
+						</picker>
+					</view>
+				</view>
+				
 				<view class="search_btn btn_style">
 					<button type="default" @tap="getTaskDetails" hover-class="btn_hover">确定</button>
 				</view>
@@ -122,6 +141,10 @@ export default {
 	},
     data() {
         return {
+			arrayState: [{"state": "全部", "key": -1}, {"state": "关闭", "key": 0}, {"state": "开启", "key": 1}],   //任务状态列表
+			arrayStateIndex: 0,
+			state: "",  //选择的任务状态码
+			
 			loadding: false, //加载数据提示
 			pullUpOn: true,  //上拉加载数据
 			rightDrawer: false,//抽屉开关
@@ -130,7 +153,6 @@ export default {
 			resultType: 0, //结果状态 0未取到数据 1有数据 2没数据
 			incomeList:[]   ,//提现明细列表
 			page: 1,  //查询页数
-			
 			
 			begTime: "", //开始时间
 			endTime: "", //结束时间
@@ -159,6 +181,20 @@ export default {
 		//获取会员等级列表
 		getLevelDesc(){
 			this.arrayLevel = storage.getLevelDescList();
+		},
+		//选择任务状态
+		statePickerChange(e){
+			this.arrayStateIndex = e.detail.value;
+			console.log(this.arrayStateIndex);
+			//状态是全部时状态码设置为""
+			if(this.arrayStateIndex == 0) {
+				this.state = "";
+				return;
+			}
+			this.arrayState.forEach((item, index) =>{
+				 //获取选中的任务状态码
+				if(this.arrayStateIndex == index) this.state = item.key;
+			});
 		},
 		//选择任务等级
 		levelPickerChange(e){
@@ -243,7 +279,8 @@ export default {
 				uid: this.uid,
 				page: this.page,
 				count: 20,
-				level: parseInt(this.level)
+				level: parseInt(this.level),
+				state: parseInt(this.state)
 			};
 		if(!util.isEmpty(this.begTime)){
 			let time = this.begTime + " 00:00:00";
@@ -285,9 +322,10 @@ export default {
 		
 		api.getTaskInfo({
 			uid: this.uid,
-			state: 1,
 			page: this.page,
-			count: 10
+			count: 10,
+			level: parseInt(this.level),
+			state: parseInt(this.state)
 		}, (res)=>{
 			let data = api.getData(res).data;
 			if(util.isEmpty(data)){
